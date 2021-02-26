6ba63d3b8ae4b5f96814ec812181530ef5ba1972
Rahul Sethuram <rahul@sethuram.com>
2021-02-26 10:26:22 +0000

    Initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserNode } from "@connext/vector-browser-node";
+import { ChannelSigner } from "@connext/vector-utils";
+
+// session monster economy alley thumb fun critic view quit wonder sound soldier
+
+async function run() {
+  const pk =
+    "0x2df591b7552895e90b6cd24cfde781c59e1afe38fc529399421cbd977db602fa";
+  const signer = new ChannelSigner(pk);
+
+  console.log("NODE CREATING");
+
+  let browserNode = await BrowserNode.connect({
+    signer,
+    chainProviders: {
+      4: "https://rinkeby.infura.io/v3/",
+    },
+    messagingUrl: "https://messaging.connext.network",
+  });
+
+  console.log("NODE CREATED: ", browserNode.publicIdentifier);
+
+  const setup = await browserNode.setup({
+    timeout: "100000",
+    //our router pubkey addr
+    counterpartyIdentifier:
+      "vector5wTFqAqyphMqbt5dWBoEt6KrkqBRzQeLAHEoV71HfrAcseukvr",
+    chainId: 4,
+  });
+  console.log("setup: ", setup.isError ? setup.getError() : setup.getValue());
+}
+run();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode></React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
